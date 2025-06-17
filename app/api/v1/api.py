c73070afb6bc0621a5058d5cd0e7812726703a44
JulianPDP <julianazange@gmail.com>
2025-06-16 20:43:43 -0500

    MOD: delete prefix in v1/api.py users

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -4,6 +4,6 @@ from app.api.endpoints import auth, device, user
 
 api_router = APIRouter()
 
-api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(user.router, tags=["users"])
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(device.router, prefix="/devices", tags=["devices"])
