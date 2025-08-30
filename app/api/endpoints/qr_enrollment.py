2a043bd92c5a21f1580345a54ba7b1d932e49b00
juantobon20 <60023902+juantobon20@users.noreply.github.com>
2025-08-30 00:36:44 -0500

    Update qr_enrollment.py

--- app/api/endpoints/qr_enrollment.py
+++ app/api/endpoints/qr_enrollment.py
@@ -9,9 +9,9 @@ router = APIRouter()
 async def get_qr_enrollment(enrollment_id: UUID, store_id: UUID, re_enrollment: bool):
     return {
         "android.app.extra.PROVISIONING_DEVICE_ADMIN_COMPONENT_NAME": "com.olimpo.smartpay/com.olimpo.smartpay.receivers.SmartPayDeviceAdminReceiver",
-        "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "D0vnIiqThESrh5FrMk916BbB-58WR32rh6D4BfMQlZk=",
+        "android.app.extra.PROVISIONING_DEVICE_ADMIN_SIGNATURE_CHECKSUM": "mjzAq8WTYC--JhC5v6IRavmHon8YczZuhBbPtvecyVk=",
         "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_DOWNLOAD_LOCATION": "https://appincdevs.com/enterprise/smartpay-google.apk",
-        "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "D0vnIiqThESrh5FrMk916BbB-58WR32rh6D4BfMQlZk=",
+        "android.app.extra.PROVISIONING_DEVICE_ADMIN_PACKAGE_CHECKSUM": "mjzAq8WTYC--JhC5v6IRavmHon8YczZuhBbPtvecyVk=",
         "android.app.extra.PROVISIONING_LEAVE_ALL_SYSTEM_APPS_ENABLED": True,
         "android.app.extra.PROVISIONING_LOCALE": "es_ES",
         "android.app.extra.PROVISIONING_ADMIN_EXTRAS_BUNDLE": {
